eb692ea78f2a6c13109d7d58314137707d981aaa
hemantk_altair <hemank@altair.com>
2024-05-29 18:01:28 +0530

    push latest change

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"log"
 	"net/http"
 
 	"github.com/kmrhemant916/k8s-webhooks/helpers"
@@ -9,7 +11,17 @@ import (
 
 const (
 	Config = "config/config.yaml"
+	CertFile = "/etc/webhook/certs/tls.crt"
+	KeyFile = "/etc/webhook/certs/tls.key"
 )
+
+func mustLoadCertificate(certFile string, keyFile string) tls.Certificate {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("failed to load key pair: %v", err)
+	}
+	return cert
+}
 
 func main() {	
 	c, err:= helpers.ReadConfig(Config)
@@ -17,5 +29,15 @@ func main() {
         panic(err)
 	}
 	r := routes.SetupRoutes()
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: r,
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{
+				mustLoadCertificate(CertFile, KeyFile),
+			},
+		},
+	}
+	log.Fatal(server.ListenAndServeTLS(CertFile, KeyFile))
 	http.ListenAndServe(":"+c.Service.Port, r)
 }
