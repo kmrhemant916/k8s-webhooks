caab2292370cb72a91d4c1d468c234d38239931f
hemantk_altair <hemank@altair.com>
2024-05-29 23:53:00 +0530

    update controller

--- controllers/mutate.go
+++ controllers/mutate.go
@@ -16,61 +16,56 @@ const (
 )
 
 func (app *App) Mutate(w http.ResponseWriter, r *http.Request) {
-	admissionReview := &admissionv1.AdmissionReview{}
-	err := helpers.ReadJSON(r, admissionReview)
-	if err != nil {
-		helpers.HandleError(w, r, err)
-		return
-	}
-	pod := &corev1.Pod{}
-	if err := json.Unmarshal(admissionReview.Request.Object.Raw, pod); err != nil {
-		helpers.HandleError(w, r, fmt.Errorf("unmarshal to pod: %v", err))
-		return
-	}
-	originalJSON := admissionReview.Request.Object.Raw
-	config, err:= helpers.ReadConfig(Config)
+    admissionReview := &admissionv1.AdmissionReview{}
+    err := json.NewDecoder(r.Body).Decode(admissionReview)
+    if err != nil {
+        helpers.HandleError(w, r, err)
+        return
+    }
+    pod := &corev1.Pod{}
+    if err := json.Unmarshal(admissionReview.Request.Object.Raw, pod); err != nil {
+        helpers.HandleError(w, r, fmt.Errorf("unmarshal to pod: %v", err))
+        return
+    }
+    originalJSON := admissionReview.Request.Object.Raw
+    config, err := helpers.ReadConfig(Config)
     if err != nil {
         panic(err)
-	}
-	for _, label := range config.TargetLabels {
-		if pod.Labels[label.Key] == label.Value {
-			toleration := corev1.Toleration{
-				Key:      config.Tolerations[0].Key,
-				Operator: corev1.TolerationOpEqual,
-				Value:    config.Tolerations[0].Value,
-				Effect:   corev1.TaintEffectNoSchedule,
-			}
-			pod.Spec.Tolerations = append(pod.Spec.Tolerations, toleration)
-		}
-	}
-	mutatedJSON, err := json.Marshal(pod)
-	if err != nil {
-		helpers.HandleError(w, r, fmt.Errorf("marshal pod: %v", err))
-		return
-	}
-	patch, err := jsonpatch.CreateMergePatch(originalJSON, mutatedJSON)
-	if err != nil {
-		helpers.HandleError(w, r, fmt.Errorf("create JSON patch: %v", err))
-		return
-	}
-	patchBytes, err := json.Marshal(patch)
-	if err != nil {
-		helpers.HandleError(w, r, fmt.Errorf("marshal patch: %v", err))
-		return
-	}
-	patchType := admissionv1.PatchTypeJSONPatch
-	admissionResponse := &admissionv1.AdmissionResponse{
-		UID:       admissionReview.Request.UID,
-		Allowed:   true,
-		Patch:     patchBytes,
-		PatchType: &patchType,
-	}
-	admissionReview.Response = admissionResponse
-	respBytes, err := json.Marshal(admissionReview)
-	if err != nil {
-		helpers.HandleError(w, r, fmt.Errorf("marshal admission review: %v", err))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(respBytes)
+    }
+    for _, label := range config.TargetLabels {
+        if pod.Labels[label.Key] == label.Value {
+            toleration := corev1.Toleration{
+                Key:      config.Tolerations[0].Key,
+                Operator: corev1.TolerationOpEqual,
+                Value:    config.Tolerations[0].Value,
+                Effect:   corev1.TaintEffectNoSchedule,
+            }
+            pod.Spec.Tolerations = append(pod.Spec.Tolerations, toleration)
+        }
+    }
+    mutatedJSON, err := json.Marshal(pod)
+    if err != nil {
+        helpers.HandleError(w, r, fmt.Errorf("marshal pod: %v", err))
+        return
+    }
+    patch, err := jsonpatch.CreateMergePatch(originalJSON, mutatedJSON)
+    if err != nil {
+        helpers.HandleError(w, r, fmt.Errorf("create JSON patch: %v", err))
+        return
+    }
+    patchType := admissionv1.PatchTypeJSONPatch
+    admissionResponse := &admissionv1.AdmissionResponse{
+        UID:       admissionReview.Request.UID,
+        Allowed:   true,
+        Patch:     patch,
+        PatchType: &patchType,
+    }
+    admissionReview.Response = admissionResponse
+    respBytes, err := json.Marshal(admissionReview)
+    if err != nil {
+        helpers.HandleError(w, r, fmt.Errorf("marshal admission review: %v", err))
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(respBytes)
 }
